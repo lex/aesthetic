5155ca36d9ff25effe7c5742f2632066489c253e
lex <lex@users.noreply.github.com>
2018-03-02 12:06:31 +0200

    add bootstrap, navigation bar

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,17 @@
+
+<template>
+    <b-navbar toggleable="md" type="dark" variant="info">
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+        <b-navbar-brand href="#">ａｅｓｔｈｅｔｉｃ</b-navbar-brand>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'NavigationBar',
+};
+</script>
+
+<style scoped>
+
+</style>
