ebdff02501897416eeadb56a5c074717992265fb
lex <lex@users.noreply.github.com>
2018-03-04 16:07:04 +0200

    add some tests for Aesthetic.vue

--- test/unit/specs/Aesthetic.spec.js
+++ test/unit/specs/Aesthetic.spec.js
@@ -1,8 +1,13 @@
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
 import Aesthetic from '@/components/Aesthetic';
+import aestheticize from '@/utils/aestheticizer';
 
 Vue.use(BootstrapVue);
+
+Object.defineProperty(document, 'execCommand', {
+  value: jest.fn()
+});
 
 describe('Aesthetic.vue', () => {
   it('should render correct contents', () => {
@@ -14,5 +19,49 @@ describe('Aesthetic.vue', () => {
     expect(vm.$el.querySelector('#textarea-aesthetic').placeholder).toEqual(
       'ｔｅｘｔ　ｗｉｌｌ　ａｐｐｅａｒ　ｈｅｒｅ'
     );
+  });
+
+  it('should aestheticize text', done => {
+    const Constructor = Vue.extend(Aesthetic);
+    const vm = new Constructor().$mount();
+    const textAreaInput = vm.$el.querySelector('#textarea-input');
+
+    const input = 'hello';
+    vm.text = input;
+    const expected = aestheticize(input);
+
+    Vue.nextTick(() => {
+      expect(vm.aesthetic).toEqual(expected);
+      done();
+    });
+  });
+
+  it('should copy text to clipboard', done => {
+    const Constructor = Vue.extend(Aesthetic);
+    const vm = new Constructor().$mount();
+    const textAreaInput = vm.$el.querySelector('#textarea-input');
+
+    const spy = jest.spyOn(document, 'execCommand');
+
+    Vue.nextTick(() => {
+      vm.copyToClipboard();
+      expect(spy).toBeCalledWith('copy');
+      done();
+    });
+  });
+
+  it('should keep the scrolling of input and output in sync', done => {
+    const Constructor = Vue.extend(Aesthetic);
+    const vm = new Constructor().$mount();
+    const textAreaInput = vm.$el.querySelector('#textarea-input');
+    const textAreaOutput = vm.$el.querySelector('#textarea-aesthetic');
+
+    textAreaInput.scrollTop = 15;
+    vm.text = 'yes';
+
+    Vue.nextTick(() => {
+      expect(textAreaOutput.scrollTop).toEqual(textAreaInput.scrollTop);
+      done();
+    });
   });
 });
