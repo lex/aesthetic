70fb083c1dae09ed6b10d9ed6875ddafd965ab1d
lex <lex@users.noreply.github.com>
2018-03-04 15:41:20 +0200

    add tests for aestheticizer

---
+++ test/unit/specs/aestheticizer.spec.js
@@ -0,0 +1,33 @@
+import aestheticize from '@/utils/aestheticizer';
+
+describe('aestheticizer.js', () => {
+  it('should aestheticize alphabets correctly', () => {
+    const input = 'abcdefghijklmnopqrstuvwxyz';
+    const expected = 'ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ';
+    expect(aestheticize(input)).toEqual(expected);
+  });
+
+  it('should aestheticize umlauts correctly', () => {
+    const input = 'åÅäÄöÖüÜ';
+    const expected = 'ａＡａａｏＯｕＵ';
+    expect(aestheticize(input)).toEqual(expected);
+  });
+
+  it('should aestheticize correctly with newlines', () => {
+    const input = 'a\nb\nc';
+    const expected = 'ａ\nｂ\nｃ';
+    expect(aestheticize(input)).toEqual(expected);
+  });
+
+  it('should aestheticize newlines correctly', () => {
+    const input = 'a b c';
+    const expected = 'ａ　ｂ　ｃ';
+    expect(aestheticize(input)).toEqual(expected);
+  });
+
+  it('should skip non ascii characters (excluding some umlauts)', () => {
+    const input = 'éÉë😂lit🤔fam€';
+    const expected = 'ｌｉｔｆａｍ';
+    expect(aestheticize(input)).toEqual(expected);
+  });
+});
